e1dd831b2bdf9d3c8e9ea20c79524f2bb92da17c
Ildar <student@localhost>
2021-11-23 19:41:10 +0300

    add example 009 magic

--- Example009_Magic/Program.cs
+++ Example009_Magic/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Clear();
 
-int xa = 1, ya = 1,
-    xb = 1, yb = 20,
-    xc = 30, yc = 20;
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -17,7 +17,7 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while (count < 100) ;
+while (count < 10000)
 {
     int what = new Random().Next(0, 3); // [0;3) 0 1 2
     if (what == 0)
@@ -35,7 +35,8 @@ while (count < 100) ;
         x = (x + xc) / 2;
         y = (y + yc) / 2;
     }
+
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
-    count = count + 1;
+    count++;
 }
